bf097aeee8e8458b01796d7ec25a6a571f869082
Kuldeep <kunalrajput67718@gmail.com>
2025-12-24 22:13:14 +0530

    footer component added

---
+++ NDL/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        27K+
+        <span>ARTISTS</span>
+      </div>
+      <div>
+        876K+
+        <span>ARTWORK</span>
+      </div>
+      <div>
+        20K+ <span>AUCTION</span>
+      </div>
+      <div>
+        <PiPlayCircle />
+        <span>PLAY VIDEO</span>
+      </div>
+      <div>
+        <TiSocialFacebook />
+        <FaInstagram />
+        <TfiTwitter />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
