c62892144546ed46a31cd3d37b25021414cb1807
Kuldeep <kunalrajput67718@gmail.com>
2025-12-24 23:03:07 +0530

    footer done

--- NDL/src/App.jsx
+++ NDL/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Nav from './components/nav'
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Nav/>
-    </>
-  )
+    <div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- NDL/src/components/Footer.jsx
+++ NDL/src/components/Footer.jsx
@@ -1,25 +1,39 @@
+
+import { TiSocialFacebook } from "react-icons/ti";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa";
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        27K+
-        <span>ARTISTS</span>
+    <footer className="text-white flex justify-evenly items-center bg-transparent absolute bottom-0 right-0 left-0 py-4.5">
+      <div className="flex flex-col text-[20px] font-bold leading-5 tracking-wider  border-r-2 border-gray-400/45 pr-10">
+        27K +
+        <span className="font-bold text-[8px] ml-2 tracking-wider text-gray-300">
+          ARTISTS
+        </span>
       </div>
-      <div>
+      <div className="flex flex-col text-[20px] font-bold leading-5 tracking-wider border-r-2 border-gray-400/45 pr-10">
         876K+
-        <span>ARTWORK</span>
+        <span className="font-bold text-[8px] ml-2 tracking-wider text-gray-300">
+          ARTWORK
+        </span>
       </div>
-      <div>
-        20K+ <span>AUCTION</span>
+      <div className="flex flex-col text-[20px] font-bold leading-5 tracking-wider border-r-2 border-gray-400/45 pr-10">
+        20K+{" "}
+        <span className="font-bold text-[8px] ml-2 tracking-wider text-gray-300">
+          AUCTION
+        </span>
       </div>
-      <div>
-        <PiPlayCircle />
-        <span>PLAY VIDEO</span>
+      <div className="flex items-center gap-1 border-r-2 border-gray-400/45 pr-10">
+        <span className="size-10 rounded-full border-2 flex items-center justify-center border-white/50">
+          <FaPlay size={12} />
+        </span>
+        <span className="text-[10px] font-medium tracking-widest text-white/90">PLAY VIDEO</span>
       </div>
-      <div>
+      <div className="flex items-center gap-6 text-white ">
         <TiSocialFacebook />
-        <FaInstagram />
-        <TfiTwitter />
+        <FaInstagram className="text-white/60" />
+        <FaTwitter />
       </div>
     </footer>
   );
